9f9316496a17613c4492dcc2a98474b78ba62b9e
zhaobin <qq1013446945@126.com>
2020-09-02 09:48:02 +0800

    添加收货地址

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,6 +19,9 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+require('./components/SelectDistrict');
+require('./components/UserAddressesCreateAndEdit');
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
