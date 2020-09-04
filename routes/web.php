e6013750d8459c93d0bb145e136a54bad497d9b5
zhaobin <qq1013446945@126.com>
2020-09-04 15:26:31 +0800

    用户界面-商品收藏列表

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Auth::routes(['verify' => true]);
 
@@ -29,4 +28,7 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 });
+
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
