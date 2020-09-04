d6359be31828ecc4378a880d052614bef7684444
zhaobin <qq1013446945@126.com>
2020-09-04 17:25:53 +0800

    商品加入购物车

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CartItem;
+use Illuminate\Http\Request;
+use App\Http\Requests\AddCartRequest;
+
+class CartController extends Controller
+{
+    public function add(AddCartRequest $request)
+    {
+        $user = $request->user();
+        $skuId = $request->input('sku_id');
+        $amount = $request->input('amount');
+
+        if ($cart = $user->cartItems()->where('product_sku_id', $skuId)->first()) {
+            $cart->update([
+                'amount' => $cart->amount + $amount,
+            ]);
+        } else {
+            $cart = new CartItem(['amount' => $amount]);
+            $cart->user()->associate($user);
+            $cart->productSku()->associate($skuId);
+            $cart->save();
+        }
+
+        return [];
+    }
+}
